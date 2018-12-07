5df81868f3c2e11c0d3a0cc7a568f30139e88ad7
MG-w <mg84@bk.ru>
2018-12-07 17:32:48 +0300

    add webpack

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+//import React, {Component} from 'react';
+//
+//class App extends Component {
+//  render() {
+//    return (
+//      <p>Test Out</p>
+//    )
+//  }
+//}
+
+function App() {
+  document.getElementById('')
+}
+
+export default App;
