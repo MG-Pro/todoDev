bbb565851806499ba5d817234721b73148398c0b
MG <mg84@bk.ru>
2018-12-08 17:19:45 +0300

    add webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,59 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
+
+const config = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: 'script.js'
+    path: path.resolve(__dirname, "./build"),
+    filename: 'bundle.js',
+    publicPath: '/assets/'
+  },
+  devServer: {
+    overlay: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+        //exclude: '/node_modules/'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../'
+            }
+          },
+          "css-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  optimization: {
+    minimize: true
   }
+
+
+};
+
+module.exports = (env, options) => {
+  const prod = options.mode === 'production';
+
+  config.devtool = prod ? false : 'source-map';
+
+  return config;
 };
