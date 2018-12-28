c84785b26cd35efb5c0d515ab95d52e51cb1e731
MG-w <mg84@bk.ru>
2018-12-28 15:07:46 +0300

    add authReducer

--- src/app/redux/actions/authentication.js
+++ src/app/redux/actions/authentication.js
@@ -1,5 +1,7 @@
-import {GET_ERRORS} from './types';
 import axios from 'axios';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+import setAuthToken from '../../helpers/setAuthToken';
+import jwt_decode from 'jwt-decode';
 
 export const registerUser = (user, history) => dispatch => {
   axios.post('/api/users/register', user)
@@ -10,12 +12,17 @@ export const registerUser = (user, history) => dispatch => {
         payload: err.response.data
       });
     });
-}
+};
 
 export const loginUser = (user) => dispatch => {
   axios.post('/api/users/login', user)
     .then(res => {
       console.log(res.data);
+      const { token } = res.data;
+      localStorage.setItem('jwtToken', token);
+      setAuthToken(token);
+      const decoded = jwt_decode(token);
+      dispatch(setCurrentUser(decoded));
     })
     .catch(err => {
       dispatch({
@@ -23,4 +30,11 @@ export const loginUser = (user) => dispatch => {
         payload: err.response.data
       });
     });
-}
+};
+
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
+};
--- src/app/redux/reducers/index.js
+++ src/app/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import tasksReducer from './tasksReducer';
 import errorReducer from './errorReducer';
+import authReducer from './authReducer';
 
 export default combineReducers({
   errors: errorReducer,
+  auth: authReducer,
   tasks: tasksReducer,
 })
